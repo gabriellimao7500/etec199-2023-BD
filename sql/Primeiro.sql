f37674a5b9be0e26244660222c9e9b4c52667e2b
gabriellimao7500 <gabriellimao7500@gmail.com>
2023-09-01 11:26:14 -0300

    add folder sql

---
+++ sql/Primeiro.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE db_produtos
+
+USE db_produtos
+
+CREATE TABLE tbl_produtos
+(
+	codigo int NOT NULL,
+	prouto varchar (20) NOT NULL,
+	data_compra date NOT NULL,
+		
+	PRIMARY KEY(codigo)
+)
+SELECT * FROM tbl_produtos
+
+INSERT INTO tbl_produtos (codigo, prouto, data_compra) VALUES
+(1,'Açucar', GETDATE()),
+(2,'Arroz',GETDATE()),
+(3,'Abacate', GETDATE()),
